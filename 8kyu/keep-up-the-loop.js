d479f7affccf906e07b2b63e59292f1513cd6605
Monae Curbeam <curbeammonae@gmail.com>
2023-09-23 14:27:52 -0400

    another solution

--- 8kyu/keep-up-the-loop.js
+++ 8kyu/keep-up-the-loop.js
@@ -12,3 +12,16 @@ function hoopCount (n) {
       return "Keep at it until you get it"
    }
    }
+
+   //other solutions
+
+   const hoopCount =(n) => {
+    if(n >= 10){
+        return "Great, now move on to tricks"
+      }else{
+        return "Keep at it until you get it"
+     }
+   }
+//
+   
+   const hoopCount =(n) =>  n < 10 ? 'Keep at it until you get it' : 'Great, now move on to tricks';
