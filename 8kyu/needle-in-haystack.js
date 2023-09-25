3faf44e48d6b06556acee2c84b7e304f0736b6b3
Monae Curbeam <curbeammonae@gmail.com>
2023-09-24 23:33:11 -0400

    solution

--- 8kyu/needle-in-haystack.js
+++ 8kyu/needle-in-haystack.js
@@ -5,3 +5,8 @@
 // After your function finds the needle it should return a message (as a string) that says:
 
 // "found the needle at position " plus the index it found the needle, so:
+
+function findNeedle(haystack) {
+    let index = haystack.indexOf('needle')
+    return `found the needle at position ${index}`
+  }
