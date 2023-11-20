06b07904203caacbf09a7760e71f90478104a436
Monae Curbeam <curbeammonae@gmail.com>
2023-11-20 17:31:57 -0500

    wide mouth frog challenge

---
+++ 8kyu/wide-mouth-frog.js
@@ -0,0 +1,10 @@
+//Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide.
+
+function mouthSize(animal) {
+    let newA = animal.toLowerCase()
+    if(newA === 'alligator'){
+      return 'small'
+    }else{
+      return 'wide'
+    }
+  }
