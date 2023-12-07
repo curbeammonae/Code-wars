d3756e8da865748b88290daf358ebee86877bf2e
Monae Curbeam <curbeammonae@gmail.com>
2023-12-07 10:56:58 -0500

    add coding challenge for 12-7-23

---
+++ 8kyu/light-sabers.js
@@ -0,0 +1,13 @@
+/*
+The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+Note: your function should have a default parameter.
+*/
+
+function howManyLightsabersDoYouOwn(name) {
+    if(name == 'Zach'){
+      return 18
+    }else{
+      return 0
+    }
+  }
