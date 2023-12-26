8da9ff6397587fc41635e5eb0c368c05bdb7b12a
monaecurbeam98 <curbeammonae@gmail.com>
2023-12-25 23:32:03 -0500

    .reduce

---
+++ 8kyu/sum-numbers.js
@@ -0,0 +1,8 @@
+// Sum Numbers
+// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+function sum (numbers) {
+
+     numbers.reduce((a,b)=> a + b, 0)
+    
+  }  
