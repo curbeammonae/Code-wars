c423475f749a2317fa29cb6751ace45d207556ba
monaecurbeam98 <curbeammonae@gmail.com>
2023-12-31 08:50:37 -0500

    conditional/.slice method

---
+++ 8kyu/exclamation.js
@@ -0,0 +1,14 @@
+// User
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it. javascript
+
+
+function removeExclamationMark(str) {
+    if (str.endsWith('!')) {
+        return str.slice(0, -1);
+    }
+    return str;
+}
+
+// Example usage:
+const result = removeExclamationMark("Hello world!"); // Output: "Hello world"
+console.log(result);
