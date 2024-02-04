55c0453c0cda17d5e510ef54a96770f9e582d760
monaecurbeam98 <curbeammonae@gmail.com>
2024-02-04 06:53:32 -0500

    condtionals

---
+++ 8kyu/alternating-cases.js
@@ -0,0 +1,10 @@
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase.
+
+
+return this.split('').map(char => {
+    if (char === char.toUpperCase()) {
+        return char.toLowerCase();
+    } else {
+        return char.toUpperCase();
+    }
+}).join('');
