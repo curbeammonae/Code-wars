b437f546690045423fc4873d1c86042924700bc7
monaecurbeam98 <curbeammonae@gmail.com>
2024-04-10 17:07:06 -0400

    filter

---
+++ 7kyu/row-weights.js
@@ -0,0 +1,8 @@
+// Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+
+function rowWeights(array){
+    let t1 = array.filter((x, i)=>i%2==0).reduce((a,item)=>a+item,0);
+    let t2 = array.filter((x, i)=>i%2!=0).reduce((a,item)=>a+item,0);
+    
+    return [t1, t2]
+  }
