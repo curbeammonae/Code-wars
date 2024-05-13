0caa0c1a491deb02927cb15f84523560145eb7b6
monaecurbeam98 <curbeammonae@gmail.com>
2024-05-13 19:15:30 -0400

    Math.pow

---
+++ 7kyu/perfect-square.js
@@ -0,0 +1,13 @@
+// You might know some pretty large perfect squares. But what about the NEXT one?
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+
+function findNextSquare(sq) {
+    var number = Math.sqrt(sq);
+    if(Math.round(number) === number) {
+      return Math.pow(++number, 2)
+    }
+    return -1;
+  }
