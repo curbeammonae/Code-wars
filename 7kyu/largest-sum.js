e047e5f2c51342a26f3f2d6abfd89ad729790b11
monaecurbeam98 <curbeammonae@gmail.com>
2024-06-02 18:23:19 -0400

    .sort

---
+++ 7kyu/largest-sum.js
@@ -0,0 +1,8 @@
+// Given a sequence of numbers, find the largest pair sum in the sequence.
+
+
+function largestPairSum(numbers)
+{
+  numbers.sort((a,b) => b - a);
+  return numbers[0] + numbers[1];
+}
